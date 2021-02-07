0f7b9a97ba32c8494cb1508457e7c5184271fe14
Christian Goldapp <c.goldapp@tu-braunschweig.de>
2021-02-07 01:37:00 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.4.21"
     application
+    `maven-publish`
 }
 
 group = "eu.goldapp"
@@ -12,12 +13,28 @@ repositories {
     mavenCentral()
 }
 
-
-
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
 
 application {
     mainClassName = "MainKt"
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/ChrisGold/Pinnwand3")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
 }
