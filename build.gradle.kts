2f092d13bc79ca14eede15aaa814239dd12d46ad
Christian Goldapp <christian@goldapp.eu>
2021-02-09 01:01:42 +0100

    Build fat jars

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,23 @@ application {
     mainClassName = "MainKt"
 }
 
+val fatJar = task("fatJar", type = Jar::class) {
+    this.setProperty("archiveFileName", "BoardDownloader.jar")
+    manifest {
+        attributes["Implementation-Title"] = "Board Downloader"
+        attributes["Implementation-Version"] = archiveVersion
+        attributes["Main-Class"] = "MainKt"
+    }
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    with(tasks.jar.get() as CopySpec)
+}
+
+tasks {
+    "build" {
+        dependsOn(fatJar)
+    }
+}
+
 publishing {
     publications {
         create<MavenPublication>("default") {
