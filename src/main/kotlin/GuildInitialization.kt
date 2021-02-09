bc92b2df9b75826668f9cdedc0e0fb08d8782b41
Christian Goldapp <christian@goldapp.eu>
2021-02-09 01:38:06 +0100

    Skeleton coding

--- src/main/kotlin/GuildInitialization.kt
+++ src/main/kotlin/GuildInitialization.kt
@@ -1,4 +1,5 @@
 import discord4j.core.DiscordClient
+import discord4j.core.`object`.entity.Guild
 import discord4j.core.event.domain.lifecycle.ConnectEvent
 
 class GuildInitialization(discord: DiscordClient) {
@@ -9,8 +10,13 @@ class GuildInitialization(discord: DiscordClient) {
 
     fun onConnect(connectEvent: ConnectEvent){
         val client = connectEvent.client
-        client.guilds.subscribe {
-            println("Connected to Guild ${it.name}")
-        }
+        client.guilds.subscribe(::init)
+    }
+
+    fun init(guild: Guild){
+        println("Connected to Guild ${guild.name}")
+        //TODO: Register guild in database if it does not exist
+        //TODO: Ensure that the pinboard channel exists if it is set and that we have the rights to use it
+        //TODO: Initialize the Pinboard object for this guild here
     }
 }
