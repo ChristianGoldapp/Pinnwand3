6d6db0c2f5a59d605b8d4b7a1712b482515c3d0c
Christian Goldapp <christian@goldapp.eu>
2021-02-10 00:46:55 +0100

    Add placeholder for checking pinboard channel

--- src/main/kotlin/GuildInitialization.kt
+++ src/main/kotlin/GuildInitialization.kt
@@ -1,6 +1,7 @@
 import db.PinnwandGuild
 import discord4j.core.DiscordClient
 import discord4j.core.`object`.entity.Guild
+import discord4j.core.`object`.util.Snowflake
 import discord4j.core.event.domain.lifecycle.ConnectEvent
 import org.jetbrains.exposed.sql.transactions.transaction
 import reactor.core.publisher.Flux
@@ -10,14 +11,23 @@ fun GuildInitialization(discord: DiscordClient): Flux<PinnwandGuildConnection> {
 
     fun init(guild: Guild): PinnwandGuildConnection {
         println("Connected to Guild ${guild.name}")
+
         //Register guild in database if it does not exist
         transaction {
             val pinnwandGuild = PinnwandGuild.findById(guild.id.asLong()) ?: PinnwandGuild.new(guild.id.asLong()){
                 firstJoined = LocalDateTime.now()
             }
             println("Settings for Guild: $pinnwandGuild")
+
+            //Ensure that the pinboard channel exists if it is set and that we have the rights to use it
+            val pinboardChannelId = pinnwandGuild.pinboardChannel?.let { Snowflake.of(it) }
+            if(pinboardChannelId != null){
+                //TODO: If channel doesn't exist,
+                // it is not a message channel,
+                // or we do not have write permissions,
+                // reset pinboard channel setting to null
+            }
         }
-        //TODO: Ensure that the pinboard channel exists if it is set and that we have the rights to use it
         //TODO: Initialize the Pinboard object for this guild here
         return PinnwandGuildConnection(discord, guild)
     }
